2b8b14af6158b5628e3971878fe5a8d11beeab1d
Nishu <nishuptg@yahoo.com>
2025-05-08 03:56:30 +0600

    solve: problem 2

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,30 @@
     return formattedString;
   }
 
-  // test
+  // test formatString()
   console.log(formatString("Hello")); // Output: "HELLO"
   console.log(formatString("Hello", true)); // Output: "HELLO"
   console.log(formatString("Hello", false)); // Output: "hello"
+
+  function filterByRating(
+    items: { title: string; rating: number }[]
+  ): { title: string; rating: number }[] {
+    let filteredItems: { title: string; rating: number }[] = items.filter(
+      function (item) {
+        return item.rating >= 4;
+      }
+    );
+
+    return filteredItems;
+  }
+
+  // test filterByRating()
+  const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 },
+  ];
+
+  console.log(filterByRating(books));
+  // Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
 }
