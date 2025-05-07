b48f33fa415df455c6d06507843936b9bc27fc93
Nishu <nishuptg@yahoo.com>
2025-05-08 03:44:52 +0600

    solve: problem 1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 {
-  // typescript test
-  let text: string = "Hello World!";
-  console.log(text);
+  function formatString(input: string, toUpper?: boolean): string {
+    let formattedString: string;
+    if (toUpper === false) {
+      formattedString = input.toLowerCase();
+    } else {
+      // both true and undefined
+      formattedString = input.toUpperCase();
+    }
+
+    return formattedString;
+  }
+
+  // test
+  console.log(formatString("Hello")); // Output: "HELLO"
+  console.log(formatString("Hello", true)); // Output: "HELLO"
+  console.log(formatString("Hello", false)); // Output: "hello"
 }
